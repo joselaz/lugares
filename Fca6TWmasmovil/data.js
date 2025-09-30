6fa8c06954093954c37f055a1866c1ca0baa4c77
joselaz <joselazo2@gmail.com>
2025-09-30 13:34:00 -0500

    commit
    upload

---
+++ Fca6TWmasmovil/data.js
@@ -0,0 +1,246 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-fca-11-1",
+      "name": "Fca 11 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.370203821980926,
+        "pitch": 0.15851726519942133,
+        "fov": 1.1678810529903434
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3800921827236046,
+          "pitch": -0.17064733301470625,
+          "rotation": 0,
+          "target": "2-fca-11-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.811821355223164,
+          "pitch": -0.10995340314198998,
+          "title": "Entrada para la Torre de Finca 6",
+          "text": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">se debe entrar al taller para poder aceder al sitio de la torre&nbsp;</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "1-fca-11-3",
+      "name": "Fca 11 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0981022344045623,
+          "pitch": -0.10875628282998306,
+          "rotation": 0,
+          "target": "3-fca-11-5"
+        },
+        {
+          "yaw": 2.734652883385042,
+          "pitch": -0.23347502057271896,
+          "rotation": 0,
+          "target": "0-fca-11-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-fca-11-4",
+      "name": "Fca 11 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7932945329085843,
+          "pitch": -0.022659744491958733,
+          "rotation": 0,
+          "target": "1-fca-11-3"
+        },
+        {
+          "yaw": -2.2442465103597016,
+          "pitch": -0.20509170512110764,
+          "rotation": 0,
+          "target": "4-fca-11-6"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.7371367684424968,
+          "pitch": -0.24982139567585904,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">Torre de Finca 6</font></font>",
+          "text": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">Vista 360 del sitio</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "3-fca-11-5",
+      "name": "Fca 11 5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.12463511863927934,
+          "pitch": 0.05302852695882976,
+          "rotation": 0,
+          "target": "0-fca-11-1"
+        },
+        {
+          "yaw": -1.0862216983419604,
+          "pitch": -0.1418328852476627,
+          "rotation": 0,
+          "target": "4-fca-11-6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-fca-11-6",
+      "name": "Fca 11 6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7908284473966187,
+          "pitch": -0.1883029307342099,
+          "rotation": 0,
+          "target": "2-fca-11-4"
+        },
+        {
+          "yaw": -2.3104428256077814,
+          "pitch": -0.0858692209815306,
+          "rotation": 0,
+          "target": "0-fca-11-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.1980141911700102,
+          "pitch": -0.03622029611943134,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">Gabinete de masmovil&nbsp;</font></font>",
+          "text": "equipo de distribucion de servicio celular"
+        }
+      ]
+    }
+  ],
+  "name": "torre masmovil Fca6",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
