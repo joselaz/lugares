cc455180c7ed7d0b2cc4556a2939e9d3a1944a3f
joselaz <joselazo2@gmail.com>
2025-09-30 09:29:08 -0500

    commit
    upload

---
+++ bigcreek/data.js
@@ -0,0 +1,238 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-big-creek-1",
+      "name": "big creek 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.837143094998133,
+        "pitch": -0.004048159834002973,
+        "fov": 1.1678810529903434
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6050124322958776,
+          "pitch": -0.06701312360049627,
+          "rotation": 0,
+          "target": "1-big-creek-2"
+        },
+        {
+          "yaw": -1.796990475607764,
+          "pitch": -0.32862493866294784,
+          "rotation": 0,
+          "target": "2-big-creek-3"
+        },
+        {
+          "yaw": -0.9362361812567812,
+          "pitch": -0.20390684918010038,
+          "rotation": 0,
+          "target": "3-big-creek-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.1693423324607544,
+          "pitch": -0.28927828684471635,
+          "title": "Torre Big Creek",
+          "text": "la torre de big creek&nbsp;&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "1-big-creek-2",
+      "name": "big creek 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0387812868532045,
+          "pitch": -0.2407734621594777,
+          "rotation": 0,
+          "target": "0-big-creek-1"
+        },
+        {
+          "yaw": -2.3696949097080626,
+          "pitch": -0.1486281760287973,
+          "rotation": 0,
+          "target": "2-big-creek-3"
+        },
+        {
+          "yaw": -1.9697983500650196,
+          "pitch": -0.5852271420417239,
+          "rotation": 0,
+          "target": "3-big-creek-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-big-creek-3",
+      "name": "big creek 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5883018044439545,
+          "pitch": -0.24014526814443649,
+          "rotation": 0,
+          "target": "1-big-creek-2"
+        },
+        {
+          "yaw": -1.8321653601960044,
+          "pitch": -0.3019297549607174,
+          "rotation": 0,
+          "target": "0-big-creek-1"
+        },
+        {
+          "yaw": -2.3171110500680427,
+          "pitch": -0.17361354406631868,
+          "rotation": 0,
+          "target": "3-big-creek-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.0301727260817213,
+          "pitch": -0.0523379911953441,
+          "title": "caseta donde estaba la primera torre",
+          "text": "en lo que se muestra en el plano donde esta la caseta estaba la primera torre&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "3-big-creek-4",
+      "name": "big creek 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0999655729428266,
+          "pitch": -0.42011134802947936,
+          "rotation": 0,
+          "target": "0-big-creek-1"
+        },
+        {
+          "yaw": 2.0772876126599407,
+          "pitch": -0.29152965655311647,
+          "rotation": 0,
+          "target": "1-big-creek-2"
+        },
+        {
+          "yaw": 3.0376062208904866,
+          "pitch": -0.0911278814395402,
+          "rotation": 0,
+          "target": "2-big-creek-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.5672545947602146,
+          "pitch": 0.39470845705253055,
+          "title": "cerro que divide los terrenos",
+          "text": "segun el plano este punto es la division del terreno"
+        },
+        {
+          "yaw": 0.4175827122591578,
+          "pitch": -0.18625268025907893,
+          "title": "en este edificio esta les simento de la primera torre",
+          "text": "donde esta el edificio esta el punto de la primera torre segun el plano enviado"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
