90e4a2d11748cc1d275b0496ac3026e10baea43b
joselaz <joselazo2@gmail.com>
2025-10-01 17:20:08 -0500

    commit
    upload

---
+++ Bigcreek2/data.js
@@ -0,0 +1,258 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-big-creek-1",
+      "name": "big creek 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.546037405313392,
+          "pitch": -0.057918314566109075,
+          "rotation": 0,
+          "target": "1-big-creek-2"
+        },
+        {
+          "yaw": -0.9642441586651245,
+          "pitch": 0.012080605924261079,
+          "rotation": 0,
+          "target": "2-big-creek-3"
+        },
+        {
+          "yaw": -1.8441773991543773,
+          "pitch": -0.21625324748858432,
+          "rotation": 0,
+          "target": "3-big-creek-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.2161535567020003,
+          "pitch": -0.1439958515345161,
+          "title": "caseta de comunicaciones mas movil panama",
+          "text": "<div><font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">en este sitio lo que indica el plano es el lugar donde estuvo la primera antena que ya no existe en el sitio</font></font></div><div><font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">según el plano donde está ubicado la caseta está ubicado en el lote 1&nbsp;</font></font>
+<img src="https://joselaz.github.io/lugares/bigcreeK2/galeria/image003.jpg" alt="plano" width="400" height="300"></div>"
+        },
+        {
+          "yaw": -2.0920114797665317,
+          "pitch": -0.3668357244625078,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">Torre BigCreek</font></font>",
+          "text": "La torre existente se encuentra en el sitio del lote 2 como indica el plano&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "1-big-creek-2",
+      "name": "big creeK 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9914097982306096,
+          "pitch": -0.23716783857367219,
+          "rotation": 0,
+          "target": "0-big-creek-1"
+        },
+        {
+          "yaw": -1.5481276888012463,
+          "pitch": -0.22146478324277652,
+          "rotation": 0,
+          "target": "2-big-creek-3"
+        },
+        {
+          "yaw": -2.3607222105272534,
+          "pitch": 0.05807542920477715,
+          "rotation": 0,
+          "target": "3-big-creek-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.0847662531907751,
+          "pitch": 0.07215062711417275,
+          "title": "<br>",
+          "text": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">En la imagen se observa una lineal roja ese es el punto de límite del lote 1 y el lote 2</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "2-big-creek-3",
+      "name": "big creeK 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.066797015961159,
+          "pitch": 0.003396902525437895,
+          "rotation": 0,
+          "target": "3-big-creek-4"
+        },
+        {
+          "yaw": 2.0525475658817935,
+          "pitch": -0.2672351239877564,
+          "rotation": 0,
+          "target": "1-big-creek-2"
+        },
+        {
+          "yaw": 1.1470738823138564,
+          "pitch": -0.2487804567611427,
+          "rotation": 0,
+          "target": "0-big-creek-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.6608771569717087,
+          "pitch": 0.2864036049253116,
+          "title": "<div><font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">Punto limite del lote 1 y lote 2</font></font></div>",
+          "text": "punto de referencia de limite del terreno"
+        },
+        {
+          "yaw": 2.2930315533995964,
+          "pitch": -0.27457946574889114,
+          "title": "<div>Torre bigcreek en lote 2</div><div><br></div>",
+          "text": "la torre esta actualmente en el lote 2"
+        },
+        {
+          "yaw": 0.6049697669075336,
+          "pitch": 0.17598551554378972,
+          "title": "lugar donde existía la torre primera",
+          "text": "donde se encuentra la caseta de comunicaciones había una torre&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "3-big-creek-4",
+      "name": "big CreeK 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5794831637381712,
+          "pitch": -0.3079258745546838,
+          "rotation": 0,
+          "target": "0-big-creek-1"
+        },
+        {
+          "yaw": -0.5402348479130179,
+          "pitch": -0.1019989199560527,
+          "rotation": 0,
+          "target": "1-big-creek-2"
+        },
+        {
+          "yaw": -2.1265621435971482,
+          "pitch": -0.3534682068062338,
+          "rotation": 0,
+          "target": "2-big-creek-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.252545659497862,
+          "pitch": 0.15412472847157588,
+          "title": "Muro existente&nbsp;",
+          "text": "observando la continuidad de la linea roja antes había una cerca continua donde se separaban los lotes y se muestran en los planos&nbsp;"
+        }
+      ]
+    }
+  ],
+  "name": "BigCreek2",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
