822b2b0677d3a03863e3ef05d820e6b830f586fd
joselaz <joselazo2@gmail.com>
2025-10-01 20:33:35 -0500

    Update data.js

--- twbig/data.js
+++ twbig/data.js
@@ -53,8 +53,7 @@ var APP_DATA = {
           "yaw": -1.2161535567020003,
           "pitch": -0.1439958515345161,
           "title": "caseta de comunicaciones mas movil panama",
-          "text":"<div><font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">en este sitio lo que indica el plano es el lugar donde estuvo la primera antena que ya no existe en el sitio</font></font></div><div><font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">según el plano donde está ubicado la caseta está ubicado en el lote 1&nbsp;</font></font></div>
-          "<div><a href="https://joselaz.github.io/lugares/twbig/galeria/image003.jpg"target="_blank" style="background: #6c757d; color: white; padding: 6px 10px; border-radius: 3px; text-decoration: none; display: inline-block;">📊 Informe técnico completo</a></div>"
+          "text":"<div><font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">en este sitio lo que indica el plano es el lugar donde estuvo la primera antena que ya no existe en el sitio</font></font></div><div><font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">según el plano donde está ubicado la caseta está ubicado en el lote 1 <a href="https://joselaz.github.io/lugares/twbig/galeria/image003.jpg">Ver los planos</a>&nbsp;</font></font></div>"
         },
         {
           "yaw": -2.0920114797665317,
